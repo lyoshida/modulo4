6989da0e1d9abbce3ac4990a2562d3c2f7efa2b9
Luis Fernando Ozaki Yoshida <luis.yoshida@gmail.com>
2012-04-24 23:08:44 -0300

    Criados formularios para adicionar publicacoes

--- circulante/circulante/catalogo/views.py
+++ circulante/circulante/catalogo/views.py
@@ -1,1 +1,43 @@
-# Create your views here.
+# coding: utf-8
+
+from .models import Publicacao
+from django.shortcuts import render
+from .isbn import validatedISBN10
+from django.http import HttpResponseRedirect
+from .forms import PublicacaoModelForm
+from django.core.urlresolvers import reverse
+
+def busca(request):
+    erros = []
+    pubs = None
+    q=''
+    if 'q' in request.GET:
+        q = request.GET['q']
+        if not q:
+            erros.append(u'Digite um termo para a busca.')
+        elif len(q) > 20:
+            erros.append(u'Digite no máximo 20 caracteres.')
+        else:
+            isbn = validatedISBN10(q)
+            if isbn:
+                pubs = Publicacao.objects.filter(id_padrao=isbn)
+            else:                
+                pubs = Publicacao.objects.filter(titulo__icontains=q)
+    vars_template = {'erros': erros, 'q':q}
+    if pubs is not None:
+        vars_template['publicacoes'] = pubs
+        vars_template['resultados'] = True
+                
+    return render(request, 'catalogo/busca.html', vars_template)
+    
+def catalogar(request):
+    if request.method != 'POST':
+        formulario = PublicacaoModelForm()
+    else:
+        formulario = PublicacaoModelForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            titulo = formulario.cleaned_data['titulo']
+            return HttpResponseRedirect(reverse('busca')+'?q='+ titulo)
+    return render(request, 'catalogo/catalogar.html',
+        {'formulario':formulario}) 
